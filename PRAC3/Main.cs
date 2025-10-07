98c4b63bb9eb7905fafcced6a5cb845e5dcdbb26
KAGS <sergej.naumov.0606@mail.ru>
2025-10-07 13:29:29 +0700

    Добавьте файлы проекта.

---
+++ PRAC3/Main.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+
+class APP
+{
+
+    static void Main()
+    {
+        ArrayListTest();
+    }
+
+
+    public static void ArrayListTest()
+    {
+        Random rand = new Random();
+        ArrayList list = new ArrayList();
+
+        for (int i = 0; i < 5; i++)
+        {
+            list.Add(rand.NextInt64(1, 100));
+        }
+
+
+        list.Add("Пицца");
+        list.RemoveAt(2);//удаляем 3-й элемент
+        Console.WriteLine($"Количество элементов: {list.Count}");
+        Console.WriteLine("\nЭлементы необощенной коллекции:");
+
+
+        foreach (var item in list)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.WriteLine($"\nПервое вхождение слова \"пицца\": {list.IndexOf("Пицца")}"); ;
+    }
+
+    public static void QueueTest() 
+    {
+        Queue<int> queue = new Queue<int> { };
+    }
+
+}
